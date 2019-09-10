fab75d7af4046ef19360fc44367f4236a7a4b192
t-eckert <t_eckert@outlook.com>
2019-09-09 22:43:06 -0700

    Add a styled teal operation button

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <CalculatorBase />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import CalculatorBase from "./components/CalculatorBase.vue";
 
 @Component({
   components: {
-    HelloWorld
+    CalculatorBase
   }
 })
 export default class App extends Vue {}
