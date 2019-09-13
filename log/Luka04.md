8da99f691e0b0bb50486b7bdeb348b1a8df8b1c4
t-eckert <t_eckert@outlook.com>
2019-09-12 22:47:56 -0700

    Add panicky code

--- log/Luka04.md
+++ log/Luka04.md
@@ -1,6 +1,6 @@
 # Luka 04
 
-Date: 11 September 2019
+Date: 11 & 12 September 2019
 
 At the end of Luka 03, I wasn't sure whether to work on connecting the Rust and TypeScript using Wasm or organize how state is passed in the Vue application. I decided to take on the former first. I think I'm going to hit some interesting walls that might affect how I structure the Vue application.
 
@@ -56,3 +56,72 @@ If I truly can't pass a `Vec<T>`, I really only need to worry about the last two
 5. TypeScript changes the stack to read `[4.5, 3, 99.5]`.
 
 This actually seems like a better design. It minimizes the amount of state passing and gives the stack a clear owner.
+
+## Have you tried rewriting this in Rust?
+
+Instead of a `State` struct, everything in this new implementation is going to be a function. Every function will take in two numbers and return a `Result` type that will give the result of the numbers undergoing the operation or an error. For operations (+, -, *, /), there will be an `operate` function that takes in the two numbers and a string matching one of those operations then handing the state back to the TypeScript.
+
+There is only one big problem. And it's one I would have had in the earlier implementation anyway.
+
+The `+`, `-`, and `*` functions in Rust all return primitive types as if their input types are okay, there are no cases in which a primitive number cannot be returned. However, `/` has one horrific edge case: division by zero. If you are designing Rust, you have to make a choice. Either the `/` function is the only function that returns a `Result` or `Option` type, or the function panics if division by zero is attempted and the return type is primitive which matches the other functions ergonomically.
+
+In the former case, handling division would be a whole mess:
+
+``` Rust
+//                                 Type
+let a = 2                       // i32
+let b = 0                       // i32
+
+let added = 2 + 0               // i32
+let subtracted = 2 - 0          // i32
+let multiplied = 2 * 0          // i32
+let divided = 2 / 0             // Err
+
+match divided {
+    Ok(quotient) => quotient,   // i32 (or some float maybe)
+    Err(message) => message,    // String
+}
+```
+
+I mean, what? You want to unpack the quotient every time you divide? I think they made the right decision that if you try to divide by zero, division `panics!`. However, I don't know what this will do across the Wasm boundary. Here is one way I _could_ solve it:
+
+``` rust
+/// Perform the operation
+#[wasm_bindgen]
+pub fn operate(a: f64, b: f64, operation: String) -> Result<f64, String> {
+    match &operation[..] {
+        "+" => Ok(a + b),
+        "-" => Ok(a - b),
+        "*" => Ok(a * b),
+        "/" => divide(a, b),
+        _ => Err(format!(
+            "Operation string `{}` does not match any allowed operation (+, -, *, /)",
+            &operation[..]
+        )),
+    }
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, String> {
+    // Preemptively catch the division by zero panic case.
+    // See documentation for [Div](https://doc.rust-lang.org/std/ops/trait.Div.html)
+    match b {
+        0.0 => Err(String::from("Division by zero is not ok.")),
+        _ => Ok(a / b),
+    }
+}
+```
+
+Except matching on a float won't be possible in future versions of Rust. This code, on compilation gives the error: 
+
+``` text
+warning: this was previously accepted by the compiler but is being phased out; it will become a hard error in a future release!
+note: for more information, see issue #41620 <https://github.com/rust-lang/rust/issues/41620>
+```
+
+## Wrap Up
+
+I don't want to write code that won't work one day, so I'm not going to. Instead, I will need to figure out how to handle the `panic!` and pass back an `Err` to Vue. For now, I opened up [this topic](https://users.rust-lang.org/t/how-should-i-handle-f64-division-by-zero-panic-elegantly/32485) on the Rust-lang forum and am hoping someone is nice enough to help me out.
+
+Starting Commit: []()
+
+Ending Commit: []()
--- src/calculator/src/lib.rs
+++ src/calculator/src/lib.rs
@@ -1,15 +1,37 @@
 use wasm_bindgen::prelude::*;
 
+/// Perform the operation
+#[wasm_bindgen]
+pub fn operate(a: f64, b: f64, operation: String) -> Result<f64, String> {
+    match &operation[..] {
+        "+" => Ok(a + b),
+        "-" => Ok(a - b),
+        "*" => Ok(a * b),
+        "/" => divide(a, b),
+        _ => Err(format!(
+            "Operation string `{}` does not match any allowed operation (+, -, *, /)",
+            &operation[..]
+        )),
+    }
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, String> {
+    // Preemptively catch the division by zero panic case.
+    // See documentation for [Div](https://doc.rust-lang.org/std/ops/trait.Div.html)
+    match b {
+        0.0 => Err(String::from("Division by zero is not ok.")),
+        _ => Ok(a / b),
+    }
+}
+
 /// # State
 ///
 /// Contains every value on the reverse polish notation calculator stack.
-#[wasm_bindgen]
 pub struct State {
     /// State handling for the "stack" uses a Vec that is treated as a stack.
     pub stack: Vec<f32>,
 }
 
-#[wasm_bindgen]
 impl State {
     /// Creates a new instance of `State` with a stack of 4 zeros.
     pub fn new() -> State {
@@ -24,139 +46,10 @@ impl State {
         self.stack.push(value);
         State { stack: self.stack }
     }
-
-    /// Perform the operation passed as string on the last two values in the `stack`.  
-    /// Expects a string matching one of the following: `+`, `-`, `*`, `/`.
-    /// If a string is passed that doesn't match, the function will return the `State` unchanged.
-    ///
-    /// ``` rust
-    /// use calculator::State;
-    /// let state = State{ stack: vec![6.0, 2.0] };
-    /// let state_prime = state.operate("/");
-    /// assert_eq!(vec![3.0], state_prime.stack);
-    /// ```
-    ///
-    /// > The trait takes a `&str` in order to process operations from TypeScript, through Wasm, by passing operations as a TypeScript `string`.
-    ///
-    pub fn operate(mut self, operation: &str) -> State {
-        // Remove the last two values from `stack`
-        let z = self.stack.pop().unwrap();
-        let y = self.stack.pop().unwrap();
-
-        // Return a `Vec<f32>` with the matching operation performed.
-        let mut stack_tail = match operation {
-            "+" => vec![y + z],
-            "-" => vec![y - z],
-            "*" => vec![y * z],
-            "/" => vec![y / z],
-            _ => vec![y, z],
-        };
-        // Return a new instance of `State` with the `stack_tail` appended.
-        self.stack.append(&mut stack_tail);
-        State { stack: self.stack }
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_initializes() {
-        let _ = State::new();
-    }
-
-    #[test]
-    fn it_pushes() {
-        // Given
-        let expected_stack = vec![3.14159];
-        let pushed_value = 3.14159;
-
-        // When
-        let state = State { stack: vec![] };
-        let state_prime = state.push(pushed_value);
-
-        // Then
-        assert_eq!(expected_stack, state_prime.stack);
-    }
-
-    #[test]
-    fn it_adds() {
-        // Given
-        let expected_stack = vec![6.0];
-        let operation = "+";
-
-        // When
-        let state = State {
-            stack: vec![2.0, 4.0],
-        };
-        let state_prime = state.operate(operation);
-
-        // Then
-        assert_eq!(expected_stack, state_prime.stack);
-    }
-
-    #[test]
-    fn it_subtracts() {
-        // Given
-        let expected_stack = vec![-2.0];
-        let operation = "-";
-
-        // When
-        let state = State {
-            stack: vec![2.0, 4.0],
-        };
-        let state_prime = state.operate(operation);
-
-        // Then
-        assert_eq!(expected_stack, state_prime.stack);
-    }
-
-    #[test]
-    fn it_multiplies() {
-        // Given
-        let expected_stack = vec![8.0];
-        let operation = "*";
-
-        // When
-        let state = State {
-            stack: vec![2.0, 4.0],
-        };
-        let state_prime = state.operate(operation);
-
-        // Then
-        assert_eq!(expected_stack, state_prime.stack);
-    }
-
-    #[test]
-    fn it_divides() {
-        // Given
-        let expected_stack = vec![0.5];
-        let operation = "/";
-
-        // When
-        let state = State {
-            stack: vec![2.0, 4.0],
-        };
-        let state_prime = state.operate(operation);
-
-        // Then
-        assert_eq!(expected_stack, state_prime.stack);
-    }
-
-    #[test]
-    fn it_passes() {
-        // Given
-        let expected_stack = vec![2.0, 4.0];
-        let operation = "; drop tables *";
-
-        // When
-        let state = State {
-            stack: vec![2.0, 4.0],
-        };
-        let state_prime = state.operate(operation);
-
-        // Then
-        assert_eq!(expected_stack, state_prime.stack);
-    }
 }
